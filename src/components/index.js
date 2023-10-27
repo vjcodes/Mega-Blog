fbde9f4f24f6ead2e51b09241f2ad18652fa85f4
vjcodes <vinayak.kris@gmail.com>
2023-10-27 17:32:45 +0530

    -added post form and slug value -RTE.jsx

--- src/components/index.js
+++ src/components/index.js
@@ -5,5 +5,6 @@ import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
 import Button from './Button'
 import Input from './Input'
+import RTE from "./RTE";
 
-export { Header, Footer, Container, Logo, LogoutBtn, Button, Input };
+export { Header, Footer, Container, Logo, LogoutBtn, Button, Input, RTE };
