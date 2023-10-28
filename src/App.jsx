1c732c30c17a0bdb29c88fde854757941de07c45
vjcodes <vinayak.kris@gmail.com>
2023-10-28 17:45:02 +0530

    -header

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import authService from "./appwrite/auth";
 import { login, logout } from "./store/authSlice";
 import { Footer } from "./components";
 import { Outlet } from "react-router-dom";
-// import Header from "./components/header/Header"
+import Header from "./components/header/Header"
 
 function App() {
   const [loading, setLoading] = useState(true);
@@ -27,7 +27,7 @@ function App() {
   return !loading ? (
     <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
       <div className="w-full block">
-        {/* <Header /> */}
+        <Header />
         <main>
           <Outlet />
         </main>
